18901fb983a7488d4798f0a21de9fbf9be183bd0
Exequiel A. Martino <107309252+locopro022@users.noreply.github.com>
2022-12-07 19:02:24 -0300

    Update index.js

--- PI-Countries-main/api/index.js
+++ PI-Countries-main/api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   /* This is a callback function that is called when the server is listening. */
 server.listen(process.env.PORT, () => {
     console.log('Server listening at port 3001'); // eslint-disable-line no-console
